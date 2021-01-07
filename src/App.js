7591f91325607718ab4ca2f2607b3d228f6f92fa
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-01-07 23:55:13 +0100

    Add service section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./components/Home/Home";
+import ServiceSection from "./components/ServiceSection/ServiceSection";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home />
+      <ServiceSection />
     </div>
   );
 }
