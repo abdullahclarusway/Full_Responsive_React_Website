43a9a1778c7872555c4c892ffc97d8d50d93262f
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-01-08 23:51:33 +0100

    Add hero component to scroll function

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -3,23 +3,28 @@ import "./Hero.css";
 import logo from "../../assets/logo/logo2.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { Element, scroller } from "react-scroll";
 
-const Hero = () => {
- 
+const Hero = (props) => {
   const scrollToNextSection = () => {
-    alert("hello");
+    scroller.scrollTo("servicesSection", { smooth: true, duration: 1500 });
   };
+  //   const scrollToNextSection = () => {
+  //     alert("hello");
+  //   };
   return (
-    <div className="hero">
-      <img src={logo} />
-      <h1>Lighthouse</h1>
-      <p>Software Development</p>
-      <p>From the Best in the Industry</p>
-      <button>Start your Project</button>
-      <div className="hero__icon" onClick={scrollToNextSection}>
-        <FontAwesomeIcon icon={faAngleDown} color="white" />
+    <Element name="servicesSection">
+      <div className="hero">
+        <img src={logo} />
+        <h1>Lighthouse</h1>
+        <p>Software Development</p>
+        <p>From the Best in the Industry</p>
+        <button>Start your Project</button>
+        <div className="hero__icon" onClick={scrollToNextSection}>
+          <FontAwesomeIcon icon={faAngleDown} color="white" />
+        </div>
       </div>
-    </div>
+    </Element>
   );
 };
 
