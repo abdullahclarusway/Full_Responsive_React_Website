5cf801c4f5e7452c23afdfd4ee8c8ba67d6a572b
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-01-10 15:58:22 +0100

    Add card component and its style

--- src/components/ReviewSection/Card.js
+++ src/components/ReviewSection/Card.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Card.css";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Card = ({ reviewText, username, userImg }) => {
   return (
     <div className="cardContainer">
+      <div className="cardContainer__icon">
+        <FontAwesomeIcon icon={faQuoteLeft} className="icon" />
+      </div>
       <p className="cardContainer__text">{reviewText}</p>
+      <span className="cardContainer__line" />
       <div className="cardContainer__user">
         <img src={userImg} />
         <span>{username}</span>
